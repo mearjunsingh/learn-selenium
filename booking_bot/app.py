c47154fed0632336df77568447b22b4951977b98
Arjun Singh <admin@arjunsingh.com.np>
2021-10-04 17:21:22 +0545

    Minor updates in bot and programiz

--- booking_bot/app.py
+++ booking_bot/app.py
@@ -4,10 +4,10 @@ from booking.booking import Booking
 with Booking() as bot:
     bot.land_first_page()
     bot.change_currency(currency='NPR')
-    bot.select_place_to_go(place='Kathmandu')
-    bot.select_dates(check_in_date='2021-09-25', check_out_date='2021-09-30')
+    bot.select_place_to_go(place='Pokhara')
+    bot.select_dates(check_in_date='2021-10-03', check_out_date='2021-10-05')
     bot.persons_and_room(person=5, room=3)
     bot.click_search()
-    bot.apply_filteration()
+    # bot.apply_filteration()
     bot.refresh()
     bot.report_results()
--- programiz_python_compiler/app.py
+++ programiz_python_compiler/app.py
@@ -1,18 +1,49 @@
 from programiz_python.programiz import PythonCompiler
+import random
 
 
-source_code = """
+source_code1 = """
+
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 8 
+
+# To take the input from the user
+#num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print(num_sqrt)
+
+
+"""
+
+source_code2 = """
 
 def add(a, b):
     return a + b
 
 
-print(add(3, 3))
+print(add(2, 3))
+
+"""
+
+source_code3 = """
+
+# This program prints Hello, world!
+
+print('Hello, world!')
+
 
 """
 
 
+source_codes = [source_code1, source_code2, source_code3]
+
+
 with PythonCompiler() as bot:
     bot.land_compiler()
-    bot.input_code(source_code)
-    bot.render_code()
+    while True:
+        source_code = random.choice(source_codes)
+        bot.input_code(source_code)
+        bot.render_code()
